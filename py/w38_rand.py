d642500ae55ad0cfddd79c7e8e3b858b0b4e210a
AnimriD <adas60@stuy.edu>
2024-05-28 15:15:51 -0400

    + w38

---
+++ py/w38_rand.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+print('Content-type: text/html\n')
+
+import random
+
+x = random.randrange(219,358)
+new = " "
+new += "President Trump will get "
+new += str(x)
+new += " Electoral Votes in November"
+print(new)
