cdaeb3328aebdc454d56537709e74c38490f94df
AnimriD <adas60@stuy.edu>
2024-05-28 15:43:13 -0400

    5/28

--- py/w38_rand.py
+++ py/w38_rand.py
@@ -2,18 +2,12 @@
 print('Content-type: text/html\n')
 
 import random
-print("<head>")
-print("""<meta charset="utf-8>""")
-print("<title>w38_rand.py</title>")
-print("\n")
-print("<body>")
+
 
 x = random.randrange(219,358)
 new = " "
 new += "President Trump will get "
 new += str(x)
 new += " Electoral Votes in November"
-print("<p>" + new + "</p>")
-print("</body>")
-print("</head>")
+print(new)
 
